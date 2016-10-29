c2866cac34ba8820814aca449006bdb93e1ec6e5
João Neto <joaopintoneto@gmail.com>
2016-10-29 17:06:42 -0200

    added tests

--- test/Hooks.js
+++ test/Hooks.js
@@ -1,5 +1,7 @@
 import chai from 'chai';
+import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
+chai.should();
 chai.use(sinonChai);
 
 import Hooks from '../src/Hooks';
@@ -7,8 +9,7 @@ import Hooks from '../src/Hooks';
 describe('# Hooks', function () {
   it('should trigger pre/post hooks', function () {
     class Model {
-      save(a) {
-        console.log(a);
+      save() {
         return { success: true };
       }
     }
@@ -16,14 +17,25 @@ describe('# Hooks', function () {
     const model = new Model();
     const modelHooks = new Hooks(model);
 
-
+    const preSave1 = sinon.spy();
+    const preSave2 = sinon.spy();
     modelHooks
-      .pre('save', () => { console.log('antes de salvar doc #1') })
-      .pre('save', () => { console.log('antes de salvar doc #2') })
-      .post('save', () => { console.log('depois de salvar doc #1') })
-      .post('save', () => { console.log('depois de salvar doc #2') });
+      .pre('save', preSave1)
+      .pre('save', preSave2);
+
+    const postSave1 = sinon.spy();
+    const postSave2 = sinon.spy();
+    modelHooks
+      .post('save', postSave1)
+      .post('save', postSave2);
 
     model.save('Salvo com sucesso');
+
+    preSave1.should.have.been.calledOnce;
+    preSave2.should.have.been.calledOnce;
+
+    postSave1.should.have.been.calledOnce;
+    postSave2.should.have.been.calledOnce;
   });
 
 });
